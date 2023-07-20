be0ce504987290e1058ae023571f4747b623e6c5
BSBMteam <43587109+BSBMteam@users.noreply.github.com>
2023-07-20 17:09:09 -0400

    Update GreatWave-Overlay.js

--- Wave 2023/GreatWave-Overlay.js
+++ Wave 2023/GreatWave-Overlay.js
@@ -2,12 +2,12 @@
 // @name         r/placeTheWave (Based off the code from r/placeDE Template)
 // @namespace    http://tampermonkey.net/
 // @version      5
-// @description  try to take over the canvas!
+// @description  try to take over the canvas! https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
-// @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
+// @updateURL    https://raw.githubusercontent.com/BSBMteam/r-place-GreatWave/main/Wave%202023/GreatWave-Overlay.js
+// @downloadURL  https://raw.githubusercontent.com/BSBMteam/r-place-GreatWave/main/Wave%202023/GreatWave-Overlay.js
 // ==/UserScript==
 
 var overlayImage = null;
